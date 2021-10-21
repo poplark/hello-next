6bfd4b6735271735e3207e3502b16c6d8b39d0ab
poplark <songkang666@126.com>
2021-10-21 16:22:21 +0800

    fix: remove getInitialProps

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,16 +4,6 @@ import 'antd/dist/antd.css';
 import Navigator from '../components/Navigator';
 
 export default class MyApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
-
   render() {
     const { Component, pageProps } = this.props
     return <>
--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -2,6 +2,7 @@ import { Card } from 'antd';
 
 function Post(props) {
   const { post } = props;
+  console.log('sssss ', post);
   return (
     <div>
       <Card title={post.title}>
@@ -26,6 +27,7 @@ Post.getInitialProps = async function getStaticProps(ctx) {
 }
 */
 
+/*
 export async function getServerSideProps(ctx) {
   const { params } = ctx;
   const resp = await fetch(`http://localhost:3000/api/posts/${params.id}`);
@@ -39,6 +41,43 @@ export async function getServerSideProps(ctx) {
     props: {
       post: result,
     }
+  }
+}
+*/
+
+export async function getStaticProps(ctx) {
+  const { params } = ctx;
+  console.log('cccc ', params)
+  const resp = await fetch(`http://localhost:3000/api/posts/${params.id}`);
+  if (!resp.ok) {
+    return {
+      notFound: true,
+    }
+  }
+  const result = await resp.json();
+  return {
+    props: {
+      post: result,
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  // const resp = await fetch('http://localhost:3000/api/posts');
+  // if (resp.ok) {
+  //   const posts = await resp.json();
+  //   return {
+  //     paths: posts.map((post) => (
+  //       { params: { id: `${post.id}` } }
+  //     )),
+  //     fallback: false,
+  //   }
+  // }
+  return {
+    paths: [
+      { params: { id: '1' } }
+    ],
+    fallback: 'blocking'
   }
 }
 
